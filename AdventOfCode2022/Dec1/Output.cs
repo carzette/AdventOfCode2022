90060f4b9a088f4bba6b75fad124294b3d852938
caze1 <caze1@DESKTOP-EJRH1G2>
2022-12-01 23:56:05 +0100

    Add project files.

---
+++ AdventOfCode2022/Dec1/Output.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class Dec1
+{
+    public static string HighestKCal()
+    {
+        var input = File.ReadAllLines(@"D:\Git\AdventOfCode2022\AdventOfCode2022\Dec1\Input.txt").ToList();
+
+        List<int> elfList = new List<int>();
+        int elf = 0;
+        foreach(var e in input)
+        {
+            if (e != "")
+            {
+                elf += int.Parse(e);
+            }
+            else
+            {
+                elfList.Add(elf);
+                elf = 0;
+            }
+        }
+        return "Högst är: " + elfList.Max();
+    }
+
+    public static string HighestThree()
+    {
+
+        var input = File.ReadAllLines(@"D:\Git\AdventOfCode2022\AdventOfCode2022\Dec1\Input.txt").ToList();
+
+        List<int> elfList = new List<int>();
+        int elf = 0;
+        foreach (var e in input)
+        {
+            if (e != "")
+            {
+                elf += int.Parse(e);
+            }
+            else
+            {
+                elfList.Add(elf);
+                elf = 0;
+            }
+        }
+        elfList.Sort();
+        var end = elfList.Count;
+        return "Högsta 3 är: " + (elfList[end-1] + elfList[end - 2] + elfList[end - 3]);
+    }
+}
+
